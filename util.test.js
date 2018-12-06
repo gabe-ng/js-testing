208d9b41ee1d058aa8e2cfb4e2bfa328ca6168cd
Gabriel Ng <gabrielng15@gmail.com>
2018-12-06 12:35:09 -0800

    Added integration test example (#4)

--- util.test.js
+++ util.test.js
@@ -1,5 +1,6 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
+// Unit test
 test('should output name and age', () => {
     const text = generateText('Max', 29);
     expect(text).toBe('Max (29 years old)');
@@ -8,4 +9,10 @@ test('should output name and age', () => {
 test('should output data-less text', () => {
     const text = generateText('', null);
     expect(text).toBe(' (null years old)');
+})
+
+// Integration test
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('Max', 29);
+    expect(text).toBe('Max (29 years old)');
 })
